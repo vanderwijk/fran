0ac809de118acc9b67c908d20abcb791121e1e35
Johan van der Wijk <vanderwijk@users.noreply.github.com>
2017-10-24 22:22:12 +0200

    Entry meta on pages

--- content-page.php
+++ content-page.php
@@ -1,5 +1,12 @@
 
 <div <?php post_class( 'block' ); ?>>
+	<div class="entry-meta">
+		<span class="date updated"><?php the_modified_date( get_option( 'date_format' ) ); ?></span>
+		<span class="vcard author">
+			<?php _e( 'by', 'fran' ); ?>
+			<span class="fn"><?php the_author(); ?></span>
+		</span>
+	</div>
 	<h1 class="entry-title"><?php the_title(); ?></h1>
 	<div class="entry-content">
 		<?php the_content(); ?>

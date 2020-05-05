6d1b29fdbefc0efbc045925bbac9eecfe606b19b
Johan van der Wijk <johan@vanderwijk.nl>
2020-05-05 17:23:19 +0200

    First fix nav menu

--- header.php
+++ header.php
@@ -2,13 +2,13 @@
 <html <?php language_attributes(); ?>>
 <head>
 <!--
-    ╭──────────────────────────────────────────╮
-    │ ≡ The Fran theme is developed by         │
-    ╞══════════════════════════════════════════╡
-    │ Johan van der Wijk - The Web Works       │
-    ├──────────────────────────────────────────┤
-    │  https://thewebworks.nl                  │
-    ╰──────────────────────────────────────────╯
+	╭──────────────────────────────────────────╮
+	│ ≡ The Fran theme is developed by         │
+	╞══════════════════════════════════════════╡
+	│ Johan van der Wijk - The Web Works       │
+	├──────────────────────────────────────────┤
+	│  https://thewebworks.nl                  │
+	╰──────────────────────────────────────────╯
 -->
 <meta charset='<?php bloginfo( 'charset' ); ?>' />
 <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0' />
@@ -21,38 +21,40 @@
 
 <body <?php body_class(); ?>>
 	<header class="header" id="header" itemscope="itemscope" itemtype="http://schema.org/WPHeader">
-		<div class="branding" id="branding">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-<?php $custom_logo_id = get_theme_mod( 'custom_logo' );
-$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-if ( has_custom_logo() ) { ?>
-				<img src="<?php echo esc_url( $logo[0] ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" class="logo" />
-<?php } else if ( is_singular() ) { ?>
-				<h2 class="site-title"><?php bloginfo( 'name' ); ?></h2>
-<?php } else { ?>
-				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-<?php } ?>
-			</a>
-			<h3 class="site-description"><?php bloginfo( 'description' ); ?></h3>
+		<div class="wrap">
+			<div class="branding" id="branding">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<?php $custom_logo_id = get_theme_mod( 'custom_logo' );
+					$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+					if ( has_custom_logo() ) { ?>
+						<img src="<?php echo esc_url( $logo[0] ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" class="logo" />
+					<?php } else if ( is_singular() ) { ?>
+						<h2 class="site-title"><?php bloginfo( 'name' ); ?></h2>
+					<?php } else { ?>
+						<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+					<?php } ?>
+				</a>
+			</div>
+			<div class="navigation">
+				<button class="menu-toggle" id="menu-toggle"><?php _e( 'Menu', 'fran' ); ?></button>
+				<nav class="nav" id="nav" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+					<?php wp_nav_menu( array( 'theme_location' => 'header', 'container' => false ) ); ?>
+				</nav>
+				<?php if ( !get_theme_mod ( 'show_search' ) == '' ) { ?>
+					<form class="searchform" id="searchform" method="get" action="<?php echo home_url(); ?>/">
+					<div class="searchwrap">
+						<input type="text" placeholder="<?php _e('Search', 'fran'); ?>" name="s" />
+						<input type="submit" value="&#xf179;" />
+					</div>
+				</form>
+			<?php } ?>
 		</div>
-		<button class="menu-toggle" id="menu-toggle"><?php _e( 'Menu', 'fran' ); ?></button>
-		<nav class="nav" id="nav" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
-			<?php wp_nav_menu( array( 'theme_location' => 'header', 'container' => false ) ); ?>
-		</nav>
-		<?php if ( !get_theme_mod ( 'show_search' ) == '' ) { ?>
-			<form class="searchform" id="searchform" method="get" action="<?php echo home_url(); ?>/">
-				<fieldset>
-					<input type="text" placeholder="<?php _e('Search', 'fran'); ?>" name="s" />
-					<input type="submit" value="&#xf179;" />
-				</fieldset>
-			</form>
-<?php } ?>
-<?php if ( !get_theme_mod ( 'show_login' ) == '' ) {
-	if ( is_user_logged_in() ) { ?>
-		<a href="<?php echo wp_logout_url(); ?>" title="<?php _e('Logout', 'fran'); ?>" class="login-logout-link"><?php _e('Logout', 'fran'); ?></a>
-	<?php } else { ?>
-		<a href="<?php echo wp_login_url( get_permalink() ); ?>" title="<?php _e('Login', 'fran'); ?>" class="login-logout-link"><?php _e('Login', 'fran'); ?></a>
-	<?php }
-} ?>
+		<?php if ( !get_theme_mod ( 'show_login' ) == '' ) {
+			if ( is_user_logged_in() ) { ?>
+				<a href="<?php echo wp_logout_url(); ?>" title="<?php _e('Logout', 'fran'); ?>" class="login-logout-link"><?php _e('Logout', 'fran'); ?></a>
+			<?php } else { ?>
+				<a href="<?php echo wp_login_url( get_permalink() ); ?>" title="<?php _e('Login', 'fran'); ?>" class="login-logout-link"><?php _e('Login', 'fran'); ?></a>
+			<?php }
+		} ?>
 	</header>
 	<div class="main" id="main" role="main">
